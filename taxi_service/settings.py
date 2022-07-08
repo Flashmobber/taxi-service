7c2c362f8856fc1cb0c713d48b8c1fcb98581924
Flashmobber <ufinaid@gmail.com>
2022-07-08 17:28:15 +0300

    Upgrade deploy files

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJ_KEY",
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DJ_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1",  "taxi-service-deploy.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
